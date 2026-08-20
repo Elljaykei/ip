2895cae86eb9e94b648f33a3d3377655b1fe79f7
Elljaykei <elljaykei@gmail.com>
2026-08-20 20:36:12 +0800

    Store entered tasks and support numbered list output

--- src/main/java/Orbit.java
+++ src/main/java/Orbit.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -18,6 +20,7 @@ public class Orbit {
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<String> tasks = new ArrayList<>();
         System.out.println(LINE);
         System.out.println(BANNER);
         System.out.println(" Hello! I'm Orbit");
@@ -30,7 +33,14 @@ public class Orbit {
                 System.out.println(LINE);
                 break;
             }
-            System.out.println(" " + input);
+            if (input.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks.get(i));
+                }
+            } else {
+                tasks.add(input);
+                System.out.println(" added: " + input);
+            }
             System.out.println(LINE);
         }
     }
